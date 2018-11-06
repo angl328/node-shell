32f8f45d51ea070c7f9c0ca61e5bb6beca2ca1d1
Sara Dornblaser <saradornblaser@gmail.com>
2018-11-06 14:23:45 -0500

    bash

---
+++ bash.js
@@ -0,0 +1,15 @@
+process.stdout.write('prompt > ');
+
+process.stdin.on('data', (data) => {
+    const cmd = data.toString().trim();
+    process.stdout.write(`${process.cwd()}`);
+    //process.stdout.write('You typed: ' + cmd);
+    process.stdout.write('\nprompt > ');
+});
+
+
+
+
+
+
+
